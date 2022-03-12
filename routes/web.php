512aa0c4a5b5aa84657fbad164748794445441fc
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-12 22:32:48 +0330

    Add new route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/pizzas', function () {
+    return view('pizzas', ['name' => 'Amir', 'age' => 18]);
+});
