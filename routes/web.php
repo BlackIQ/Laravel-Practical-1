0976040e130d4afbd1ee6c9d1c703b2d811fb406
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-13 01:00:08 +0330

    Learn passing id

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{id}', function ($id) {
+    // $id = request('id');
+    return view('welcome', ['id' => $id]);
 });
 Route::get('/pizzas', function () {
-    return view('pizzas', ['name' => 'Amir', 'age' => 18]);
+    return view('pizzas', [
+        'name' => 'Amir',
+        'age' => 18,
+    ]);
 });
