e5ca46d732ba651c4dd09dd282291268bb6260b9
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-13 01:00:12 +0330

    Show id

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    <h1>Welcome to Laravel</h1>
+    <h1>Welcome to {{ $id }}</h1>
 </div>
 @endsection
