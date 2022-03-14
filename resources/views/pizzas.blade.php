063b3a1a8534a8afc334194d854e69682b3bfa9a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-14 08:28:14 +0330

    This page, render an id that we pass in url

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -2,26 +2,6 @@
 
 @section('content')
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    @if (Route::has('login'))
-        <div class="fixed top-0 right-0 px-6 py-4 sm:block">
-            @auth
-                <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-            @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                @endif
-            @endauth
-        </div>
-    @endif
-
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 text-center">
-        <h1>Pizzas</h1>
-        <br>
-        @for ($i = 0; $i < 5; $i++)
-            <p>{{ $i }}</p>
-        @endfor
-    </div>
+    <h1>Pizzas</h1>
 </div>
 @endsection
