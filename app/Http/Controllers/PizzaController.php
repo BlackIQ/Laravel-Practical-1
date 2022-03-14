b2cc45899ec194aab69b214ada187a10084b93f8
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-14 08:27:31 +0330

    Create PizzaController

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PizzaController extends Controller
+{
+    public function index() {
+        return view('pizzas');
+    }
+    public function show($id) {
+        return view('pizza', ['id' => $id]);
+    }
+}
