eb504da9437424c804334a84828b94e87fcadd5e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-14 08:28:25 +0330

    Pizza page

---
+++ resources/views/pizza.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.laylout')
+
+@section('content')
+<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <h1>Pizza code {{ $id }}</h1>
+</div>
+@endsection
