63d20a9277dc71acecf6af20f47f6543e365423a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-16 01:43:10 +0330

    Add toppings

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -2,11 +2,18 @@
 
 @section('content')
 <div>
-    <h1>Client</h1>
+    <h1 class="text-danger">Client</h1>
     <p>For <b>{{ $pizza->name }}</b></p>
     <hr>
-    <h1>Pizza</h1>
-    <p>Base {{ $pizza->base }}</p>
-    <p>Type {{ $pizza->type }}</p>
+    <h1 class="text-danger">Pizza</h1>
+    <p>Base <b>{{ $pizza->base }}</b></p>
+    <p>Type <b>{{ $pizza->type }}</b></p>
+    <hr>
+    <h1 class="text-danger">Drink</h1>
+    <ul>
+        @foreach ($pizza->toppings as $drink)
+            <li>{{ $drink }}</li>
+        @endforeach
+    </ul>
 </div>
 @endsection
