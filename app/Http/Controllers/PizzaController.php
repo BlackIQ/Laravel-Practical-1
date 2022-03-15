e80ef64822a7075674753660826f0cd7ff6d0ae2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-16 01:42:53 +0330

    Add toppings

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -32,6 +32,7 @@ class PizzaController extends Controller
         $pizza->name = request('name');
         $pizza->type = request('type');
         $pizza->base = request('base');
+        $pizza->toppings = request('toppings');
         $pizza->price = 15;
 
         $pizza->save();
