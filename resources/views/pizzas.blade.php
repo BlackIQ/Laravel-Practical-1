9bb1cc7848e61dd39ced66f0b8a0d14a92ce9ba1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-15 22:13:37 +0330

    Remove some files

--- resources/views/pizzas.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.laylout')
-
-@section('content')
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    <h1>Pizzas</h1>
-</div>
-@endsection
