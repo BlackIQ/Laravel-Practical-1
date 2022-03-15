824fedd45a87b7a24efb0e197533d19a078842ad
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-15 22:13:04 +0330

    Add create

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pizza;
 
 class PizzaController extends Controller
 {
     public function index() {
-        return view('pizzas');
+        // $pizzas = Pizza::all();
+        // $pizzas = Pizza::orderBy('name', 'desc')-> get();
+        // $pizzas = Pizza::where('type', 'Normal')->get();
+        $pizzas = Pizza::latest()->get();
+
+        return view('pizzas.index', ['pizzas' => $pizzas]);
     }
 
     public function show($id) {
-        return view('pizza', ['id' => $id]);
+        return view('pizzas.show', ['id' => $id]);
+    }
+
+    public function create() {
+        return view('pizzas.create');
     }
 }
