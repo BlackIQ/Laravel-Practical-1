f9f424bd7905223df363752fa362ad5e1e411b1d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-15 20:44:13 +0330

    Add pizzas table

---
+++ database/migrations/2022_03_15_170007_create_pizzas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pizzas', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('base');
+            $table->string('name');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pizzas');
+    }
+};
