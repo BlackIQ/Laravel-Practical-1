041f694adbe454df82e84beddc190d46eb200cb2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-16 01:43:29 +0330

    Add toppings col

--- database/migrations/2022_03_15_170007_create_pizzas_table.php
+++ database/migrations/2022_03_15_170007_create_pizzas_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('base');
             $table->string('name');
             $table->integer('price');
+            $table->json('toppings');
             $table->timestamps();
         });
     }
