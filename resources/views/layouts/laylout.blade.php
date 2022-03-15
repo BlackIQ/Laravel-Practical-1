20032f3f8a8ec34dbc7d130e513c7ba39fbe803c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-16 00:57:32 +0330

    Add new files

--- resources/views/layouts/laylout.blade.php
+++ resources/views/layouts/laylout.blade.php
@@ -4,10 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel app</title>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="/css/main.css" rel="stylesheet">
+        <link href="/css/style.css" rel="stylesheet">
+        <link href="/css/mdb.min.css" rel="stylesheet">
     </head>
-    <body class="antialiased">
-        @yield('content')
+    <body class="">
+        <div class="container">
+            @yield('content')
+        </div>
+        <script src="/js/jquery-3.6.0.min.js"></script>
+        <script src="/js/script.js"></script>
+        <script src="/js/mdb.min.js"></script>
     </body>
 </html>
