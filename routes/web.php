cb6b3fca7cb357dc737f7685b3a58c7eaf6a8ea4
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-16 02:04:17 +0330

    Handle delete method

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/pizzas', 'App\Http\Controllers\PizzaController@index');
 Route::post('/pizzas', 'App\Http\Controllers\PizzaController@store');
 Route::get('/pizzas/create', 'App\Http\Controllers\PizzaController@create');
 Route::get('/pizzas/{id}', 'App\Http\Controllers\PizzaController@show');
+Route::delete('/pizzas/{id}', 'App\Http\Controllers\PizzaController@destroy');
