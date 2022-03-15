1ea0015992148d81071f2075b8a9ee6279588e94
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-16 00:58:22 +0330

    Stuff

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.laylout')
 
 @section('content')
-<div class="">
-    <h1>Pizza code {{ $id }}</h1>
+<div class="" style="padding: 2%;">
+    <h1>Client</h1>
+    <p>For <b>{{ $pizza->name }}</b></p>
+    <hr>
+    <h1>Pizza</h1>
+    <p>Base {{ $pizza->base }}</p>
+    <p>Type {{ $pizza->type }}</p>
 </div>
 @endsection
