605f798089ac9b61fe634ca9da538cb12dbbef44
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-16 00:58:00 +0330

    Remove classes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.laylout')
 
 @section('content')
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+<div>
     <h1>Welcome to Pizza House</h1>
 </div>
 @endsection
